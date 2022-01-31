6a035f11af6303fb438ecdbd40cd42d95c6472a7
Akhil <akhilu5042@gmail.com>
2022-01-31 22:26:25 +0530

    UC-1_ValidateFirstName

--- UserRegistration_Lambda/Program.cs
+++ UserRegistration_Lambda/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using UserRegistration_Lambda;
 
 namespace UserRegistrationUsingLambda
 {
@@ -7,6 +9,17 @@ namespace UserRegistrationUsingLambda
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Reg using Lambda Expressions!");
+            Console.WriteLine("Enter First Name :");
+            string name = Console.ReadLine();
+            //reference fot regex class
+            RegeX regex = new RegeX();
+            //if regex is valid 
+            //it exexutes as valid firstname
+            //otherwise invalid firstname
+            if (regex.ValidFirstName(name))
+                Console.WriteLine("First Name is VALID !!");
+            else
+                Console.WriteLine("First Name is INVALID !!");
         }
     }
 }
